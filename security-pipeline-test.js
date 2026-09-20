161a5455b2578258604a9db5ab7340e2a2eae95a
Srikar T <169169726+dis-craft@users.noreply.github.com>
2026-09-20 14:43:03 +0530

    test: trigger end-to-end AI remediation

--- security-pipeline-test.js
+++ security-pipeline-test.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2");
 
 function findUser(db, username) {
-  const query = `SELECT * FROM users WHERE username = '${username}'`;
+  const query = `SELECT * FROM users WHERE username = '${username}'`; // E2E: trigger SQL injection remediation
   return db.query(query);
 }
 
