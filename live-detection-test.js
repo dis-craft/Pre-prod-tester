b5b8e231c3ab158d3542bde205691184a16045c0
Srikar T <169169726+dis-craft@users.noreply.github.com>
2026-09-20 13:35:30 +0530

    chore: remove live detection test fixture

--- live-detection-test.js
+++
@@ -1,8 +0,0 @@
-const mysql = require("mysql2");
-
-function findUser(db, username) {
-  const query = `SELECT * FROM users WHERE username = '${username}'`;
-  return db.query(query);
-}
-
-module.exports = { findUser };
